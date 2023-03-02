5faec6a9a8b04659af779bcfaa3429499cb6fa11
huynh van hoa <huynhvanhoa17011968@gmail.com>
2023-03-02 13:04:23 +0700

    commit 5

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 // import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "@/styles/globals.css";
-import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }) {
   return (
